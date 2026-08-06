4f96eef5c7dffa391e17eabf7ce56c4e4478e895
PDVMOZ <franciscaluismaquina@gmail.com>
2026-08-06 02:10:34 +0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,11 @@
 // =====================================================
 
 
-const API = "http://127.0.0.1:8000";
+const API =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+        ? "http://127.0.0.1:8000"
+        : window.location.origin;
 
 
 // =====================================================
